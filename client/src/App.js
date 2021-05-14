f4d0ed59e70f150fc0181229b807caad7b56bd8f
Isac Svensson <isac.m.svensson@gmail.com>
2021-05-14 23:29:12 +0200

    changed method name and removed redundant comments

--- client/src/App.js
+++ client/src/App.js
@@ -151,20 +151,19 @@ function App() {
       setMatrix(populateGrid())
   }
 
-  async function test() {
-    const url = "http://192.168.0.185:5000/dijkstra"
+  async function visualize() {
+    const url = process.env.PUBLIC_URL + "/dijkstra"
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: 'POST', 
+      mode: 'cors', 
+      cache: 'no-cache', 
+      credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(matrix) // body data type must match "Content-Type" header
+      redirect: 'follow', 
+      referrerPolicy: 'no-referrer', 
+      body: JSON.stringify(matrix) 
     });
     response.json().then(data => {
       animate(data.visitedNodesInOrder, data.shortestPath)
