188607ae745b961a034405eb3c9f9fc2d51302d2
Isac Svensson <isac.m.svensson@gmail.com>
2021-05-21 13:25:36 +0200

    added button for changing animationspeed

--- client/src/components/OptionsRow.js
+++ client/src/components/OptionsRow.js
@@ -1,15 +1,24 @@
+import { useState } from 'react'
 import Button from './Button'
 import Slider from './Slider'
 
-function optionsRow({reset, visualize, clear, setAnimationSpeed, animationSpeed}) {
+function OptionsRow({reset, visualize, clear, setAnimationSpeed, animationSpeed}) {
+    const [showSlide, setShowSlide] = useState(false)
+    const showSlider = () => setShowSlide(!showSlide)
+
+    let slider ="";
+
+    if (showSlide)
+        slider = <Slider setSpeed={setAnimationSpeed} animationSpeed={animationSpeed}/>
     return (
         <div className="opt-row">
-            <Button text="Clear" color="red" onClick={clear}></Button>
+            <Button text="Clear" onClick={clear}></Button>
             <Button text="Reset" onClick={reset}></Button>
             <Button text="Visualize" color="green" onClick={visualize}></Button>
-            <Slider setSpeed={setAnimationSpeed} animationSpeed={animationSpeed}/>
+            <Button text="Animation Speed" onClick={showSlider}></Button>
+            {slider}
         </div>
     )
 }
 
-export default optionsRow
+export default OptionsRow
